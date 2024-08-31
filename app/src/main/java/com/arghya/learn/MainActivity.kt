7000b4c3b528fae621e0377e5e2c3f5d34918e40
Arghya20 <arghyaghosh.vlog@gmail.com>
2024-08-31 22:09:15 +0530

    change web link

--- app/src/main/java/com/arghya/learn/MainActivity.kt
+++ app/src/main/java/com/arghya/learn/MainActivity.kt
@@ -2,13 +2,18 @@ package com.arghya.learn
 
 import android.animation.ValueAnimator
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -25,14 +30,20 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val btnIntent = findViewById<Button>(R.id.btnIntent)
+        val webView = findViewById<WebView>(R.id.webView)
+        webViewSetup(webView)
 
-        btnIntent.setOnClickListener {
-            intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
+    }
+
+    private fun webViewSetup(webView: WebView) {
+
+        webView.webViewClient = WebViewClient()
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+            loadUrl("https://bluelearn.com")
         }
-
-
     }
 
 
